9c3175b1a594e25960fa91bbe8b3011c98829e71
Aleksey <a.abrosimow2012@yandex.ru>
2018-09-26 22:02:21 +0300

    Доработки главной страницы

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	
+  	$('.responsive').slick({
+    dots: true,
+    infinite: true,
+    adaptiveHeight: true,
+    prevArrow: '<img class="prev-left" src="img/prev-left.png">',
+    nextArrow: '<img class="prev-right" src="img/prev-right.png">',
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          adaptiveHeight: true,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          adaptiveHeight: true,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.party-news-sliders-block').slick({
+    customPaging: function (slider, i)
+    {
+      return i + 1;
+    },
+    dots:true,
+    appendDots:$('.party .news-slider-btns .party-news-dots .party .news-slider-btns2 .party-news-dots'),
+    });
+});
