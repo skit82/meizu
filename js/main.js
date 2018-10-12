122e61d1ebd4d97a856fd561011b51e29704ea3b
Aleksey <a.abrosimow2012@yandex.ru>
2018-10-12 21:59:54 +0300

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-    
+    $.ajaxSetup({cache: true});
+
+    var loadCSS = function(url) {
+      var ms=document.createElement("link");ms.rel="stylesheet";
+      ms.href=url;document.getElementsByTagName("head")[0].appendChild(ms);
+    }
+        
 
     $('[data-toggle]').on('click', function(e){
       var tgt = $(this).data('target');
@@ -11,58 +17,6 @@ $(document).ready(function() {
         return false;
       }
     })
-
-    $("[data-href]").on("click", function() {
-      href = $(this).data("href");
-      window.location.href = href;
-    });
-	 
-    if ($('[data-src]').length > 0) {
-        !function(e) {
-            function t(e, t) {
-                var n = new Image
-                  , r = e.getAttribute("data-src");
-                n.onload = function() {
-                    e.parent ? e.parent.replaceChild(n, e) : e.src = r,
-                    t && t()
-                }
-                ,
-                n.src = r
-            }
-            for (var n = new Array, r = function(e, t) {
-                if (document.querySelectorAll)
-                    t = document.querySelectorAll(e);
-                else {
-                    var n = document
-                      , r = n.styleSheets[0] || n.createStyleSheet();
-                    r.addRule(e, "f:b");
-                    for (var l = n.all, o = 0, c = [], i = l.length; o < i; o++)
-                        l[o].currentStyle.f && c.push(l[o]);
-                    r.removeRule(0),
-                    t = c
-                }
-                return t
-            }("img.lazy"), l = function() {
-                for (var r = 0; r < n.length; r++)
-                    l = n[r],
-                    void 0,
-                    (o = l.getBoundingClientRect()).top >= 0 && o.left >= 0 && o.top <= (e.innerHeight || document.documentElement.clientHeight) && t(n[r], function() {
-                        n.splice(r, r)
-                    });
-                var l, o
-            }, o = 0; o < r.length; o++)
-                n.push(r[o]);
-            l(),
-            function(t, n) {
-                e.addEventListener ? this.addEventListener(t, n, !1) : e.attachEvent ? this.attachEvent("on" + t, n) : this["on" + t] = n
-            }("scroll", l)
-        }(this);
-    }
-    
-    $('.official__btn').on('click', function() {
-        $(this).hide();
-        $('.official__more').fadeIn();
-    });
     
   	$('.service__list').slick({
     dots: true,
